abc241f9469411dcd18a125c27a1e860c682eacd
Ju young Jeon <sorajjy@naver.com>
2021-08-31 09:55:43 +0900

    Add BOJ problem:17838(Implementation, Linked List) SOLVED

--- baekjoon/17837/newgame2.cpp
+++ baekjoon/17837/newgame2.cpp
@@ -12,18 +12,16 @@ piece* p[10];
 int n, k;
 
 bool move(piece* cur){
-	cout << "CURR:" <<cur->idx<<'\n';
 	int ny = cur->y+dir[cur->dir][0];
 	int nx = cur->x+dir[cur->dir][1];
 	if(map[ny][nx]==2||ny<=0||ny>n||nx<=0||nx>n){ //change dir when blue or out of map
-		cout <<"BLUE\n";
 		if(cur->dir%2==0) cur->dir+=1;
 		else cur->dir-=1;
 		ny = cur->y+dir[cur->dir][0];
 		nx = cur->x+dir[cur->dir][1];
-		if(map[ny][nx]==2||ny<0||ny>n||nx<0||nx>n) return false;
-	}else if(map[ny][nx]==1){ // change order when red
-		cout<<"RED\n";
+		if(map[ny][nx]==2||ny<=0||ny>n||nx<=0||nx>n) return false;
+	}
+	if(map[ny][nx]==1){ // change order when red
 		piece * t=cur->next;
 		if(cur->prev!=NULL)cur->prev->next=NULL;
 		cur->prev=NULL;
@@ -34,13 +32,12 @@ bool move(piece* cur){
 			cur=cur->prev;
 			t = cur->next;
 		}
-		t=cur;
 	}
 	// MOVE + combine
 	if(cur->prev!=NULL) cur->prev->next=NULL;
 	cur->prev = NULL;
 	int cnt =0;
-	for(int i=0;i<n; i++){
+	for(int i=0;i<k; i++){
 		if(p[i]->y==ny&&p[i]->x==nx) {
 			piece* t = p[i];
 			while(t->next!=NULL) {t=t->next;}
@@ -51,13 +48,7 @@ bool move(piece* cur){
 	}
 	piece*t=cur->prev;
 	while(t!=NULL) {cnt+=1;t=t->prev;}
-	for(int i=0;i<n; i++){
-		piece*t = p[i];
-		while(t!=NULL) {cout << t->idx<<' '; if(t->next==NULL) break; t=t->next;}cout<<'b' <<'\n';
-		while(t!=NULL) {cout << t->idx<<' '; if(t->prev==NULL) break; t=t->prev;}cout <<'\n';
-	}
 	while(cur!=NULL) {cur->y = ny; cur->x = nx; cur=cur->next;cnt+=1;}
-	cout << "OUT\n"<<cnt<<'\n';
 	return cnt>=4;
 }
 
@@ -82,9 +73,9 @@ int main(void){
 	while(ans<1001&&!endCond){
 		for(int i=0;i<k; i++){
 			endCond=move(p[i]);
+			if(endCond) break;
 		}
 		ans+=1;
-		cout <<"CNT"<< ans<<'\n';
 	}
 	if(ans > 1000 ) cout <<"-1\n";
 	else cout << ans;
