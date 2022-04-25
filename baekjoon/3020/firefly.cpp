25147c3bba938ea11986338021fe8625f796c6b9
JuyoungJeon <sorajjy@naver.com>
2022-04-25 12:51:51 +0900

    Add BOJ prolem 3020: Firefly(Binary Search)

--- baekjoon/3020/firefly.cpp
+++ baekjoon/3020/firefly.cpp
@@ -1,35 +1,32 @@
 #include<iostream>
+#include<algorithm>
 
 using namespace std;
 
-int obs[200000];
+int top[100000];
+int bot[100000];
 
 int main(void){
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int N, H;
-	cin >> N >> H;
-	for(int i=0;i<N; i++){
-		cin >> obs[i];
-	}
-	int minObs=N;
-	int numMin=0;
-	for(int i=0;i<H; i++){
-		int nobs=0;
-		for(int j=0;j<N; j++){
-			if(j%2==0){
-				if(H-obs[j]<=i) nobs+=1;
-			}else{
-				if(obs[j]>i) nobs+=1;
-			}
-		}
-		if(nobs<minObs){
-			numMin=1;
-			minObs=nobs;
-		}else if (nobs==minObs){
-			numMin+=1;
-		}
-	}
-	cout << minObs <<' '<<numMin;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N, H;
+    cin >> N >> H;
+    for(int i=0;i<N/2; i++){
+        cin >> bot[i];
+        cin >> top[i];
+    }
+    sort(bot, bot+N/2);
+    sort(top, top+N/2);
+    int ans=N;
+    int ansN=0;
+    for(int i=1;i<=H; i++){
+        int t=N/2-(lower_bound(bot, bot+N/2, i)-bot);
+        t+=N/2-(lower_bound(top, top+N/2, H-i+1)-top);
+        if(t<ans){
+            ans=t; ansN=1;
+        }else if(t==ans){
+            ansN+=1;
+        }
+    }
+    cout << ans<< ' '<<ansN;
 }
